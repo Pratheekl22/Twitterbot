ba22e17ea18f0f12040e8b0664d3c68d58480865
Pratheekl22 <pratheek.lakkireddy47@gmail.com>
2020-11-02 11:50:11 -0500

    Added documentation and helper method. Tweet when followed now rt's when needed.

--- bot1.js
+++ bot1.js
@@ -1,11 +1,14 @@
-// Wordnik key and some inflection things
+// Wordnik key and inflection things
 var WordnikAPIKey = '0nxyph37juaxtz2c5haq6i8wam1dl5wmykp0dbdxdwts25z6l';
 var request = require('request');
 var inflection = require('inflection');
 var pluralize = inflection.pluralize;
 var capitalize = inflection.capitalize;
 var singularize = inflection.singularize;
-var preMadeReplies;	// store prebuilt strings here.
+
+// Global premade replies for the bot
+var preMadeReplies;
+var preMadeAnimalReplies;
 
 // Blacklist
 var wordfilter = require('wordfilter');
@@ -17,15 +20,68 @@ var Twit = require('twit');
 var fs = require('fs');
 var path = require('path');
 
+//File reader to read animals line by line
+var readline = require('readline');
+
 // Include configuration file
 var T = new Twit(require('./config.js'));
 
-// Picks random item from array
+Array.prototype.pick = function() {
+    return this[Math.floor(Math.random()*this.length)];
+}
+
+/**
+ * Gets random element from an array
+ * @param arr array to extract random element from
+ * @returns {*} randomly selected element from the array
+ */
 function randomFromArray(arr){
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
+/**
+ * Gathers all animals of one type in an array
+ * Randomly chooses one of the animals to return
+ * @param arr the array of animals
+ * @param animal to choose a random from
+ * @returns {*} A random animal of type animal
+ */
+function randomAnimalFromArray(arr, animal){
+    let specAnimal = [];
+    arr.forEach(function (animalArr) {
+        if (animalArr.includes(animal)) {
+            specAnimal.push(animalArr);
+        }
+    })
+
+    // This will be the address of the animal in the images file
+    return randomFromArray(specAnimal);
+}
+
+/**
+ * Removes integers from file names in images
+ * @param str the string to remove an int from
+ * @returns {string} the string with no ints
+ */
+function removeInts(str) {
+    let rtn = "";
+    // Due to the naming convention of the file, going backwards is efficient
+    for (let i = str.length - 1; i <= 0; i--) {
+        if(isNaN(str.charAt(i).parseInt)) {
+            rtn += str.charAt(i);
+        }
+
+        return rtn;
+    }
+}
+
+/**
+ * Creates and posts a tweet with an image and status
+ * @param content the location of the image to upload
+ * @param tweetText the status of the tweet
+ */
 function tweetWithImage(content, tweetText) {
+    // First upload the image to Twitter
     T.post('media/upload', {media_data: content}, function (err, data, response) {
         if (err) {
             console.log('Error uploading: ', err);
@@ -47,12 +103,30 @@ function tweetWithImage(content, tweetText) {
     })
 }
 
+/**
+ * Verifies the item provided is an animal
+ * @param item the item to check
+ */
+function isAnimal(item) {
+    var rd = readline.createInterface({
+        input: fs.createReadStream('C:\\Users\\Pratheek Lakkireddy\\IdeaProjects\\Twitterbot\\animals'),
+        output: process.stdout,
+    });
+
+    rd.on('line', function(line) {
+        if (item.includes(line.toLowerCase())) {
+            return line;
+        }
+    })
+}
+
 //Since tweet is the first thing that runs, images will be defined and available to all methods
 var images = [];
 // Posts images
 //ToDo Perhaps provide the name of the animal
 function tweet() {
     console.log("Tweet event");
+    // Fill images[] with image paths from the images file
     fs.readdir(__dirname + '/images', function (err, files) {
         if (err) {
             console.log("Error: " + err);
@@ -63,31 +137,60 @@ function tweet() {
             });
 
             //Select a random image from the array and store its path
-            const img = path.join(__dirname, '/images/' + randomFromArray(images)),
+            let animal = randomFromArray(images);
+            const img = path.join(__dirname, '/images/' + animal),
                 content = fs.readFileSync(img, {encoding: 'base64'});
             let tweetText = preMadeReplies.pick();
 
-            tweetWithImage(content, tweetText);
+            //tweetWithImage(content, tweetText);
             //Upload the image to twitter
-
         }
     })
 }
 
 
-//TODO retweet when an animal image is provided
+//TODO stop @ when replied to
+// Check if the user specified what animal they want to see
 function respondToMention(tweet) {
     console.log("Mention event");
     //Select a random image from the array and store its path
-    const img = path.join(__dirname, '/images/' + randomFromArray(images)),
-        content = fs.readFileSync(img, {encoding: 'base64'});
+
     let mentioner = '@' + tweet.user.screen_name;
+    let reply = "";
+    //if the bot detects an image is provided, it will consider retweeting it
+    if (tweet.profile_image_url !== '') {
+        console.log("User media detected");
+        //Verify the image provided is an animal we recognize
+        let x = isAnimal(tweet.text.toLowerCase());
+        console.log(x);
+        // If x is undefined, we do not know if the tweet text contains an animal
+        if (typeof x !== undefined) {
+            console.log("Animal detected");
+            reply = mentioner + " " + preMadeAnimalReplies.pick();
+            let params = {
+                id: tweet.id_str,
+                status: reply
+            }
 
-    reply = mentioner + " " + preMadeReplies.pick();
+            T.post('statuses/retweet/:id', params, function (err, data, response) {
+                if (err) {
+                    console.log('Error retweeting: ', err);
+                } else {
+                    console.log("Status retweet successful!");
+                }
+            })
+        }
+    } else {
+        reply = mentioner + " " + preMadeReplies.pick();
+        const img = path.join(__dirname, '/images/' + randomFromArray(images)),
+            content = fs.readFileSync(img, {encoding: 'base64'});
 
-    tweetWithImage(content, reply);
+        tweetWithImage(content, reply);
+    }
 }
 
+//ToDo make this function work
+// check if user has favorite animal, if they do show them that animal
 function tweetWhenFollowed(follow) {
     console.log("Follow event");
     //Select a random image from the array and store its path
@@ -117,6 +220,8 @@ function getSyn(word) {
     })
 }
 
+//ToDo Maybe add an animal of the day method that posts a picture of the most
+// talked about animal of that day, if we dont have one just say what it is
 function runBot() {
     console.log(" "); // just for legible logs
 
@@ -125,6 +230,12 @@ function runBot() {
             "How's it going! You seem like you could use more animal " + "picture" + " in your life",
             "Who " + "loves" + " animals? I hope you do!"
         ];
+
+    preMadeAnimalReplies = [
+        "Hello" + " I hope you " + "enjoy" + " this animal picture!",
+        "How's it going! You seem like you could use more animal " + "picture" + " in your life",
+        "Who " + "loves" + " animals? I hope you do!"
+    ];
 
     tweet();
     var stream = T.stream('statuses/filter', { track: '@AnimalBot444' });
