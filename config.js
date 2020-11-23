4d33b0e1c1aa25066536af9696505509da790cc4
Pratheekl22 <pratheek.lakkireddy47@gmail.com>
2020-11-22 20:21:59 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'U25DSlxfhPvAzA9OORvJu6eLt',
+  consumer_secret:      'Ye51oWsm5pdhbYkYzQJzupwWrLmqgvU5GWZh2IBFdhOEFsRwrg',
+  access_token:         '1213522970926149632-PhWj6TCf64E1FNClFlL3Bh411IbFvh',
+  access_token_secret:  'I506yrblBVFvw2tTpoy0rEUiwjIgR7PsIHzeK2LNuv6w8'
+}
